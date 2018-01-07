d7951f0b263ea90d78f14b0a6d2c1d7026cdb4a8
bielakarolina <bielakarolina77@gmail.com>
2018-01-07 18:42:28 +0100

    Update RepoCommits.java

--- src/main/java/app/analysis/RepoCommits.java
+++ src/main/java/app/analysis/RepoCommits.java
@@ -2,7 +2,7 @@ package app.analysis;
 
 import app.fetch.Fetcher;
 import app.structures.CommitDetails;
-import app.structures.GUIDetails;
+import app.structures.ModuleNames;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
@@ -14,63 +14,73 @@ import org.joda.time.Hours;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class RepoCommits extends AbstractAnalyzerModule {
     private static int numberOfIntervals = 5;
 
-    private List<DateTime> dateTimeList;
-    private DateTime projectStartDate;
-    private DateTime projectEndDate;
-    private Fetcher fetcher;
+    private List<CommitDetails> commits;
+    private DateTime projectStartDate = DateTime.now().minusYears(1), projectEndDate = DateTime.now();
 
-    public RepoCommits(){
-     //   fetcher = new Fetcher();
-//        this.dateTimeList = createDemoData();
-        //jak dziewczyny dodadza swoj modul to tutaj trzeba bedzie dodac import i to odkomentowac, a to na gorze zakomentowac
-//        this.dateTimeList = fetcher.getCommitsDates();
-    }
+        private List<DateTime> dateTimeList;
+//    private DateTime projectStartDate;
+//    private DateTime projectEndDate;
+//    private Fetcher fetcher;
 
-    private List<DateTime> createDemoData(){
-        DateTime tmpStartDate = new DateTime(1996, 7, 14, 0, 0);
-        List<DateTime> trialList = new ArrayList<DateTime>();
-        for (int i = 0; i < 10; i++){
-            trialList.add(tmpStartDate);
-            tmpStartDate = tmpStartDate.plusDays(1);
+    private int RepoCommits(int year, int month) {
+
+        HashSet<DateTime> dateSet = new HashSet<>();
+        List<CommitDetails> commitsForYearAndMonth =  commits.stream()
+               .filter(x -> x.getCommitDate().getYear() == year && x.getCommitDate().getMonthOfYear() == month)
+               .collect(Collectors.toList());
+        for(CommitDetails comDetails: commitsForYearAndMonth){
+            DateTime date = comDetails.getCommitDate();
+            dateSet.add(date);
         }
-        return trialList;
+
+        return dateSet.size();
+
     }
 
-    @Override
-    public String toString() {
-        return "Repository commit details";
-    }
+//    private List<DateTime> createDemoData(){
+//        DateTime tmpStartDate = new DateTime(1996, 7, 14, 0, 0);
+//        List<DateTime> trialList = new ArrayList<DateTime>();
+//        for (int i = 0; i < 10; i++){
+//            trialList.add(tmpStartDate);
+//            tmpStartDate = tmpStartDate.plusDays(1);
+//        }
+//        return trialList;
+//    }
 
     @Override
-    public File generateFile(List<CommitDetails> commitDetails, GUIDetails guiDetails) throws Exception {
-        this.commitDetails = commitDetails;
-        this.from = guiDetails.getFrom();
-        this.to = guiDetails.getTo();
-        this.dateTimeList = this.commitDetails.stream().map(CommitDetails::getCommitDate).collect(Collectors.toList());
-        return createDiagram();
+    public ModuleNames getModuleName() {
+        return ModuleNames.MODULE2;
+    }
+
+    public String generateFile(List<CommitDetails> commitDetails) throws Exception {
+       // this.dateTimeList = data.stream().map(CommitDetails::getCommitDate).collect(Collectors.toList());
+        commits = commitDetails;
+
+        return "file:"+createDiagram();
     }
 
     private XYDataset createDataset(){
-        XYSeries series = new XYSeries("Number of commitDetails in whole project.");
-
-        projectStartDate = dateTimeList.get(0);
-        projectEndDate = dateTimeList.get(dateTimeList.size() - 1);
+        XYSeries series = new XYSeries("Number of commits in whole project.");
 
         int interval = (Hours.hoursBetween(projectStartDate, projectEndDate).getHours() ) / numberOfIntervals;
         DateTime nextInterval = projectStartDate.plusHours(interval);
 
-        int numberOfCommits = 0;
+        int numberOfCommits;
         int hourOfProjectExistance = 0;
-        for (DateTime dt : dateTimeList) {
-            numberOfCommits++;
-            if (nextInterval.compareTo(dt) < 0){
+
+        for(int i=0, year = projectStartDate.getYear();year <= projectEndDate.getYear(); year++){
+            for(int month = year != projectStartDate.getYear() ? 1 : projectStartDate.getMonthOfYear();
+                month <= (year != projectEndDate.getYear() ? 12 : projectEndDate.getMonthOfYear());
+                month++, i++){
                 hourOfProjectExistance+=interval;
+                numberOfCommits=RepoCommits(year,month);
                 series.add(hourOfProjectExistance, numberOfCommits);
                 nextInterval = nextInterval.plusHours(interval);
             }
@@ -81,16 +91,16 @@ public class RepoCommits extends AbstractAnalyzerModule {
         return dataset;
     }
 
-    private File createDiagram(){
-        String path = "images/repoCommits.jpg";
+    private String createDiagram(){
+        String path = ModuleNames.getPathForOutput(ModuleNames.MODULE2);
         int width = 640;
         int height = 480;
 
         XYDataset dataset = createDataset();
         JFreeChart chart = ChartFactory.createScatterPlot(
-                "Number of commitDetails in a project dependent on hour of the project existance.",
+                "Number of commits in a project dependent on hour of the project existance.",
                 "Hour of project existance",
-                "Number of commitDetails",
+                "Number of commits",
                 dataset);
 
         File outputFile = new File(path);
@@ -101,6 +111,6 @@ public class RepoCommits extends AbstractAnalyzerModule {
             System.err.println("Problem occurred creating chart.");
         }
 
-        return outputFile;
+        return path;
     }
 }
