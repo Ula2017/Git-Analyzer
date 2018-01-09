5ade9fb5dc44b404a01100743ef49bfbdde0731d
basiachra <basia.chra@gmail.com>
2018-01-09 16:33:01 +0100

    Add module 4

--- src/main/java/app/iOCModule.java
+++ src/main/java/app/iOCModule.java
@@ -3,12 +3,15 @@ package app;
 import app.analysis.AbstractAnalyzerModule;
 import app.analysis.MonthlyAmmountOfCommitersModule;
 import app.analysis.ProgrammingLanguagesPercentageModule;
+import app.analysis.RankingModule;
 import app.analysis.RepoCommitsModule;
 import app.fetch.GitDownloader;
 import app.fetch.RepoDownloader;
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
 import com.google.inject.multibindings.Multibinder;
 
+@Singleton
 public class iOCModule extends AbstractModule {
     @Override
     protected void configure() {
@@ -16,8 +19,20 @@ public class iOCModule extends AbstractModule {
         analyzerModuleMultibinder.addBinding().to(MonthlyAmmountOfCommitersModule.class);
         analyzerModuleMultibinder.addBinding().to(ProgrammingLanguagesPercentageModule.class);
         analyzerModuleMultibinder.addBinding().to(RepoCommitsModule.class);
-
+        analyzerModuleMultibinder.addBinding().to(RankingModule.class);
         bind(RepoDownloader.class).to(GitDownloader.class);
 
     }
 }
+//@Retention(RetentionPolicy.RUNTIME)
+//@Target({ElementType.FIELD, ElementType.PARAMETER})
+//@BindingAnnotation
+//@interface RepoUrl {}
+
+//    private String url;
+//    public iOCModule(String url){
+//        this.url=url;
+//    }
+
+//        bindConstant().annotatedWith(RepoUrl.class).to(url);
+//        AnnotatedBindingBuilder<MainMenuController> bind = bind(MainMenuController.class);
