f38a5abc29dc15e6d54d49b561e6e9d18473bbb4
Agata Bogacz <agatabogacz@outlook.com>
2018-01-13 10:00:45 +0100

    Changes in RepoCommitsModule

--- src/main/java/app/analysis/RepoCommitsModule.java
+++ src/main/java/app/analysis/RepoCommitsModule.java
@@ -1,5 +1,4 @@
 package app.analysis;
-
 import app.structures.CommitDetails;
 import app.structures.GUIDetails;
 import org.jfree.chart.ChartFactory;
@@ -10,70 +9,83 @@ import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.joda.time.DateTime;
 import org.joda.time.Hours;
-
 import java.io.File;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//import app.structures.ModuleNames;
-
 public class RepoCommitsModule extends AbstractAnalyzerModule {
-    private static int numberOfIntervals = 5;
-
-    private List<CommitDetails> commits;
+    private static int numberOfIntervals = 10;
+    private List<DateTime> commitsDateTimeList;
+    private List<DateTime> commitsInPeriodOfTimeList;
     private DateTime projectStartDate = DateTime.now().minusYears(1), projectEndDate = DateTime.now();
 
+	@Override
+	public String toString() {
+		return "Commits in repository";
+	}
+   
     @Override
     public File generateFile(List<CommitDetails> commitDetails, GUIDetails guiDetails) throws Exception {
         return createDiagram(commitDetails, guiDetails.getFrom(), guiDetails.getTo());
     }
+       
+    private int createListWithCommits(List<CommitDetails> commitDetails) {
+    	commitsDateTimeList = new ArrayList<DateTime>();
+              
+        for(CommitDetails comDetails: commitDetails)
+        	commitsDateTimeList.add(comDetails.getCommitDate());        
 
-    private int RepoCommits(List<CommitDetails> commitDetails, int year, int month) {
-        HashSet<DateTime> dateSet = new HashSet<>();
-        List<CommitDetails> commitsForYearAndMonth =  commitDetails.stream()
-               .filter(x -> x.getCommitDate().getYear() == year && x.getCommitDate().getMonthOfYear() == month)
-               .collect(Collectors.toList());
-        for(CommitDetails comDetails: commitsForYearAndMonth){
-            DateTime date = comDetails.getCommitDate();
-            dateSet.add(date);
-        }
-
-        return dateSet.size();
-
+    	return commitsDateTimeList.size();
     }
-
-    private XYDataset createDataset(List<CommitDetails> commitDetails){
+    
+    
+    private int findCommitsInPeriodOfTime(DateTime startDate, DateTime endDate) {
+    	commitsInPeriodOfTimeList = new ArrayList<DateTime>();			//czy na pewno tworzy sie nowa lista
+        List<DateTime> commitsListTmp = commitsDateTimeList.stream()
+        		.filter(x -> x.compareTo(startDate) >= 0 && x.compareTo(endDate) < 0) //x >= startDate && x < endDate <- musi sie miescic w przedziale czasu
+                .collect(Collectors.toList());        
+          
+        for (int i = 0; i < commitsListTmp.size(); i++)
+        	commitsInPeriodOfTimeList.add(commitsListTmp.get(i));
+        
+    	return commitsInPeriodOfTimeList.size();
+    }
+    
+    
+    private XYDataset createDataset(){
         XYSeries series = new XYSeries("Number of commits in whole project.");
 
         int interval = (Hours.hoursBetween(projectStartDate, projectEndDate).getHours() ) / numberOfIntervals;
-        DateTime nextInterval = projectStartDate.plusHours(interval);
-
-        int numberOfCommits;
-        int hourOfProjectExistance = 0;
-
-        for(int i=0, year = projectStartDate.getYear();year <= projectEndDate.getYear(); year++){
-            for(int month = year != projectStartDate.getYear() ? 1 : projectStartDate.getMonthOfYear();
-                month <= (year != projectEndDate.getYear() ? 12 : projectEndDate.getMonthOfYear());
-                month++, i++){
-                hourOfProjectExistance+=interval;
-                numberOfCommits=RepoCommits(commitDetails, year,month);
-                series.add(hourOfProjectExistance, numberOfCommits);
-                nextInterval = nextInterval.plusHours(interval);
-            }
+        DateTime periodStartDate = projectStartDate;
+        int hourOfProjectExistance = 0;      
+        int numberOfCommitsInPeriod = 0;
+        
+        while (periodStartDate.compareTo(projectEndDate) < 0){				//periodStartDate < projectEndDate
+        	numberOfCommitsInPeriod += findCommitsInPeriodOfTime(periodStartDate, periodStartDate.plusHours(interval));
+        	series.add(hourOfProjectExistance, numberOfCommitsInPeriod);
+        	periodStartDate = periodStartDate.plusHours(interval);
+        	hourOfProjectExistance+=interval;
         }
-
+        
         XYSeriesCollection dataset = new XYSeriesCollection();
         dataset.addSeries(series);
         return dataset;
-    }
-
+    }   
+    
+    
     private File createDiagram(List<CommitDetails> commitDetails, DateTime from, DateTime to){
         String path = getPathForOutput();
         int width = 640;
         int height = 480;
+             
+        int res = createListWithCommits(commitDetails);
+        if (res < 1){
+        	//jesli pusta lista to psuje sie diagram, do poprawy 
+        }
+      
 
-        XYDataset dataset = createDataset(commitDetails);
+        XYDataset dataset = createDataset();
         JFreeChart chart = ChartFactory.createScatterPlot(
                 "Number of commits in a project dependent on hour of the project existance.",
                 "Hour of project existance",
@@ -90,6 +102,4 @@ public class RepoCommitsModule extends AbstractAnalyzerModule {
 
         return outputFile;
     }
-
-
 }
